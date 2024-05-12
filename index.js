73ee8f11a2d2e82ddff9ec772f7f45876e834d1e
tungbeo345 <meteoraitonam1311@outlook.com>
2024-05-12 16:35:34 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
     }
+    max = Math.max(max, dp[i]);
   }
-  return dp[0];
+  return max;
 }
